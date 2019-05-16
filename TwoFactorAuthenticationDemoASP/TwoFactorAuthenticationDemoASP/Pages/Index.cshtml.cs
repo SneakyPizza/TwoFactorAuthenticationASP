319903f704c1376a1bf628e9d72f5a4a60d7c542
Yornie <jinxie88@live.nl>
2019-05-16 13:41:29 +0200

    Cleaning + Start programming
    - Cleaning template code
    - Start HTML
    - Password hashing

--- TwoFactorAuthenticationDemoASP/TwoFactorAuthenticationDemoASP/Pages/Index.cshtml.cs
+++ TwoFactorAuthenticationDemoASP/TwoFactorAuthenticationDemoASP/Pages/Index.cshtml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+using System.Web;
 
 namespace TwoFactorAuthenticationDemoASP.Pages
 {
@@ -13,5 +15,25 @@ namespace TwoFactorAuthenticationDemoASP.Pages
         {
 
         }
+
+        private void LoginButtonClick(object sender, EventArgs e)
+        {
+            string un = Request.Form["tb_Username"].ToString();
+            string pw = Request.Form["tb_Password"].ToString();
+            LoginUser(un, pw);
+        }
+
+        private void LoginUser(string username, string password)
+        {
+            if(!string.IsNullOrWhiteSpace(username)&& !string.IsNullOrWhiteSpace(password))
+            {
+
+            } else
+            {
+                Console.WriteLine("not validated");
+                
+            }
+        }
+
     }
 }
