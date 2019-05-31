b5a26bd585796ee3a694f783d0458c0b6622a75a
Yornie <jinxie88@live.nl>
2019-05-31 11:01:54 +0200

    remake asp core mvc project start by Barry

---
+++ TwoFADemo/TwoFADemo/Models/Loginmodel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TwoFADemo.Models
+{
+    public class Loginmodel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
